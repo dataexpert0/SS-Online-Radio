773df6a33cc918aeff12fd83099472d16b7212fe
Gustavo Marcato Goulart <gustavom1283@gmail.com>
2025-03-24 16:49:22 -0300

    Seven Sins Radio Player and Landing Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { PlayerComponent } from './components/player/player.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, AboutComponent, PlayerComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'ss-radio-web';
 }
